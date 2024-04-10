e50bb9488dd984db0d7ee0627298776aca7fc3c7
aura0223 <sora03pt@yahoo.co.jp>
2024-04-11 00:27:31 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.scss';
 
+import { useState } from "react";
 import AppAppBar from './AppAppBar';
 import Title from './Title';
 import FAQ from './FAQ';
 import Example from './Example';
 import Footer from './Footer';
+import Modal from "react-modal";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 function App() {
+  const [editModalIsOpen, setEditModalIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setEditModalIsOpen(false);
+  };
+
   return (
     <div>
       <div id="top" className='main'>
@@ -21,8 +39,19 @@ function App() {
       <FAQ />
       <Footer />
       <div className='main__button'>
-        <button className='button-85'>お問い合わせ</button>
+        <button className='button-85'
+        onClick={() => {
+          setEditModalIsOpen(true);
+        }}>お問い合わせ</button>
       </div>
+      <Modal
+        isOpen={editModalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}>
+          <div className='main__modal'>
+            お問い合わせありがとうございます！！！
+          </div>
+      </Modal>
     </div>
   );
 }
